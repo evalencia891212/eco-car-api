4fc0fdfb4547c598bba2b76015562eb9f317ed02
evalencia891212 <emmanuel.valencia@42-q.com>
2024-05-28 14:05:28 -0600

    Database

--- src/controllers/index.controller.js
+++ src/controllers/index.controller.js
@@ -1,5 +1,7 @@
+import {pool} from '../db.js'
 
 
 export const ping = async (req,res) => {
-    res.json('Pong'); 
+    const [result] = await pool.query('SELECT "Pong" as result;')
+    res.json(result[0]); 
  }
