6e613a0163928a6c26764611decbf9b0a79aa3ee
evalencia891212 <emmanuel.valencia@42-q.com>
2024-05-28 14:05:15 -0600

    Database

--- src/db.js
+++ src/db.js
@@ -1,9 +1,17 @@
 import {createPool} from 'mysql2/promise';
 
-export const pool = createPool({
-    host:"localhost",
-    user:"root",
-    password:".2Evaca60",
-    port:3306,
-    database:"eco-cars"
-})
+// export const pool = createPool({
+//     host:"localhost",
+//     user:"admin",
+//     password:"heroku-pass-123",
+//     port:3306,
+//     database:"eco-cars"
+// })
+
+ export const pool = createPool({
+     host:"66a1tk.stackhero-network.com",
+     user:"root",
+     password:"nDUxIwbJZPOcqoLYctucnCb19ZSzH44a",
+     port:3306,
+     database:"eco-cars"
+ })
