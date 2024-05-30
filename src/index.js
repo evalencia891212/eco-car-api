c6cec884bed4af87b57777401bc4e24c45d8ee7c
evalencia891212 <emmanuel.valencia@42-q.com>
2024-05-29 21:59:03 -0600

    Version to VM

--- src/index.js
+++ src/index.js
@@ -35,17 +35,17 @@ app.listen(port);
 
 console.log('Server listen on port 3000');
 
-//const webApp = express();
+const webApp = express();
 
-//const path = '/usr/dev/Caso Practico/eco-car-api/src/views/';
+const path = '/usr/dev/Caso Practico/eco-car-api/src/views/';
 //const path = __dirname + '/views/';
-//webApp.use(express.static(path));
+webApp.use(express.static(path));
 
-//webApp.get('/', (req, res) => {
-//    console.log(path + 'index.html')
-//    res.sendFile(path + 'index.html');
-//});
+webApp.get('/', (req, res) => {
+   console.log(path + 'index.html')
+   res.sendFile(path + 'index.html');
+});
 
-//webApp.listen(4200)
+webApp.listen(4200)
 
-//console.log('views listen on port 4200');
+console.log('views listen on port 4200');
